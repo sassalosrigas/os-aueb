ab92f61c6f72278681f587e8cbefe09251416d4a
rigas <sassalosrigas@gmail.com>
2024-05-22 14:48:09 +0300

    gurl

---
+++ os/os/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  os
+//
+//  Created by Rigas Sassalos on 22/5/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
