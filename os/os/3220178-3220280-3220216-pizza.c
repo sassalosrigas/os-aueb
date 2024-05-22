43aad091124f2456365645f969f843cdd3a06709
rigas <sassalosrigas@gmail.com>
2024-05-22 14:50:37 +0300

    start again

---
+++ os/os/3220178-3220280-3220216-pizza.c
@@ -0,0 +1,8 @@
+//
+//  3220178-3220280-3220216-pizza.c
+//  os
+//
+//  Created by Rigas Sassalos, Anais Farhat, Alexandra Tsitsopoulou.
+//
+
+#include "3220178-3220280-3220216-pizza.h"
---
+++ os/os/3220178-3220280-3220216-pizza.h
@@ -0,0 +1,13 @@
+//
+//  3220178-3220280-3220216-pizza.h
+//  os
+//
+//  Created by Rigas Sassalos, Anais Farhat, Alexandra Tsitsopoulou.
+//
+
+#ifndef _220178_3220280_3220216_pizza_h
+#define _220178_3220280_3220216_pizza_h
+
+#include <stdio.h>
+
+#endif /* _220178_3220280_3220216_pizza_h */
